de49d88ac1feed3b3dd52a3ac4993505420ea11b
ParthSharma-2 <sparth332@gmail.com>
2026-06-21 01:55:45 +0530

    Deployed Production Code

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,14 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://finsight-ai-fawn.vercel.app",
+        "https://www.finsightai.space",
+        "https://finsightai.space",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
